8f37341c4744a28960235f3d127cda1b70e173bb
root <root@mreiss-abtt.andrew.cmu.edu>
2009-12-07 20:24:33 -0500

    Fixed up backuptoafs. TODO: keytab authentication.

--- bin/backuptoafs.sh
+++ bin/backuptoafs.sh
@@ -5,3 +5,7 @@
 cd /afs/andrew.cmu.edu/usr/abtech/sysadmins/abtt_sql_backups
 mysqldump abtechtt_prod -u abtech|gzip > $(date +%Y%m%d%H%M%S).abtt-backup.sql.gz
 
+#keep 7 recent (or so), delete rest
+#commented out because of permissions
+#ls -t|sed -n '2,$p'|xargs rm 
+

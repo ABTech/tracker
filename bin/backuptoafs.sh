ba59e50b2c1a3d73ea4fbf6c4fe3eeec74d34856
Mickey Reiss <mreiss@andrew.cmu.edu>
2010-02-08 13:24:46 -0500

    Updated backup script

--- bin/backuptoafs.sh
+++ bin/backuptoafs.sh
@@ -3,7 +3,7 @@
 #this is a cronjob
 
 cd /afs/andrew.cmu.edu/usr/abtech/sysadmins/abtt_sql_backups
-mysqldump abtechtt_prod -u abtech -pGJE8w6q7hA9s|gzip > $(date +%Y%m%d%H%M%S).abtt-backup.sql.gz
+mysqldump abtechtt_prod -u backup -pQ6HcF9fGdcjEZ37V |gzip > $(date +%Y%m%d%H%M%S).abtt-backup.sql.gz
   
 
 #keep 7 recent (or so), delete rest

d26df9dc30d53682a0d852e451a526a3a2fe76da
Nick Harper <nharper@nharper.org>
2011-08-03 23:24:23 -0700

    Removed dead minilist and arb code

--- app/models/event.rb
+++ app/models/event.rb
@@ -26,7 +26,6 @@ class Event < ActiveRecord::Base
   has_many                :journals;		
   belongs_to              :organization;
   belongs_to              :year;
-  belongs_to              :arb, :foreign_key => "arb_member_id", :class_name => "Member";
 
   has_many                :attachments
   

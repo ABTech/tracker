95125199e5c6d1c40f9476d8b46121053746c9ca
Joe Burgess <joemburgess@gmail.com>
2012-02-19 08:56:44 -0500

    Fixed Issue 16
      - It was broken because shortname was required in the DB
      - Added "New Shortname" to the newitem creator
      - Added validate_presence_of shortname

--- app/models/equipment.rb
+++ app/models/equipment.rb
@@ -14,6 +14,6 @@ class Equipment < ActiveRecord::Base
     belongs_to :parent, :foreign_key => "parent_id", :class_name => "EquipmentCategory";
     has_and_belongs_to_many :eventdates;
 
-    validates_presence_of  :description, :position, :parent_id;
+    validates_presence_of  :description, :position, :parent_id,:shortname;
     validates_associated   :parent;
 end

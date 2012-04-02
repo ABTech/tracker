0cb872a21a3320d61b9628970ad80f1a5adf4cfb
Mickey Reiss <mreiss@andrew.cmu.edu>
2012-04-02 15:05:01 -0400

    Fix Joe's crap. (rename migration with duplicate number)

--- db/migrate/094_add_category_to_journal.rb
+++
@@ -1,9 +0,0 @@
-class AddCategoryToJournal < ActiveRecord::Migration
-  def self.up
-    add_column "journals", "paymeth_category", :string, :default=>""
-  end
-
-  def self.down
-    remove_column "journals", "paymeth_category"
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 94) do
+ActiveRecord::Schema.define(:version => 95) do
 
   create_table "accounts", :force => true do |t|
     t.string  "name",      :null => false

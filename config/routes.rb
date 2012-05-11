6fa98b9fcf073af26584266c94d5fcccc4f1086d
Mickey Reiss <mreiss@andrew.cmu.edu>
2012-05-11 13:06:21 -0400

    Clean up routes a tiny bit.

--- config/routes.rb
+++ config/routes.rb
@@ -6,20 +6,6 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :members, :collection => [:edit_self]
   map.resources :accounts, :collection => {:list => :get, :unpaid => :get, :unpaid_print => :get, :events => :get}
 
-  # The priority is based upon order of creation: first created -> highest priority.
-  
-  # Sample of regular route:
-  # map.connect 'products/:id', :controller => 'catalog', :action => 'view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  # map.purchase 'products/:id/purchase', :controller => 'catalog', :action => 'purchase'
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # You can have the root of your site routed by hooking up '' 
-  # -- just remember to delete public/index.html.
-  # map.connect '', :controller => "welcome"
-
   map.connect "calendar/generate.:format", :controller => "events", :action => "generate"
   map.connect "calendar", :controller => "events", :action => "calendar";
 
@@ -27,6 +13,7 @@ ActionController::Routing::Routes.draw do |map|
   map.connect "iphone", :controller => "events", :action => "iphone";
   map.connect "i", :controller => "events", :action => "iphone";
   map.connect "invoice/email/:id", :controller =>"invoice", :action=>"email", :conditions => {:method=>:post}
+
   map.resources :organizations
   map.resources :bugs
   map.resources :tshirts
@@ -42,7 +29,7 @@ ActionController::Routing::Routes.draw do |map|
   map.connect "login", :controller => "useraccount", :action => "login"
   map.connect "logout", :controller => "useraccount", :action => "logout"
 
-  map.connect '',   :controller => "events", :action => "index" 
+  map.root :controller => "events", :action => "index" 
 
   # Install the default route as the lowest priority.
   map.connect ':controller/:action/:id'

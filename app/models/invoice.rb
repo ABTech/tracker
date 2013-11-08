d4c6eee03d3cf631d22068c62006f02bbd2b91ff
Edward Garbade <tedgarb2@gmail.com>
2013-11-08 12:18:14 -0500

    reordering in invoice

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -3,12 +3,12 @@ class Invoice < ActiveRecord::Base
   has_many :invoice_lines, :dependent => :destroy;
   has_one :journal_invoice, :class_name => "Journal", :foreign_key => "invoice_id", :dependent => :destroy;
 
+  Payment_Types = ["StuAct", "Check", "Oracle"];
+  Invoice_Status_Group_All = ["New", "Quote", "Contract","Invoice", "Received" ];
+
   validates_presence_of :status, :event, :event_id
   validates_inclusion_of :status, :in => Invoice_Status_Group_All;
   validates_associated :event;
-	
-  Payment_Types = ["StuAct", "Check", "Oracle"];
-  Invoice_Status_Group_All = ["New", "Quote", "Contract","Invoice", "Received" ];
 
   def total
     return invoice_lines.inject(0) {|sum,line| sum + line.total};

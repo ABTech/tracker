3ef51c5068758906268d4bf758c1bf1ae8244ccc
Edward <tedgarb2@gmail.com>
2013-11-08 11:58:27 -0500

    commit out buggy code in invoice items controller

--- app/controllers/invoice_items_controller.rb
+++ app/controllers/invoice_items_controller.rb
@@ -4,8 +4,8 @@ class InvoiceItemsController < ApplicationController
   end
 
   # GETs should be safe (see http://www.w3.org/2001/tag/doc/whenToUseGet.html)
-  verify :method => :post, :only => [ :destroy, :create, :update ],
-         :redirect_to => { :action => :list }
+  # verify :method => :post, :only => [ :destroy, :create, :update ],
+  #       :redirect_to => { :action => :list }
 
   def list
     @invoice_items = InvoiceItems.find(:all)

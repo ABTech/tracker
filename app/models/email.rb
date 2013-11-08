127fafeec41b92757928ea6eb0109e505f90fe8c
Edward Garbade <tedgarb2@gmail.com>
2013-11-08 12:10:58 -0500

    reorder stuff in email model

--- app/models/email.rb
+++ app/models/email.rb
@@ -1,10 +1,6 @@
 class Email < ActiveRecord::Base
   belongs_to :event
 
-  validates_presence_of :sender, :timestamp, :contents, :status; #:contactemail
-  validates_inclusion_of :status, :in => Email_Status_Group_All;
-  validates_format_of :sender, :with => Event::EmailRegex;
-
   Email_Status_Unfiled  = "Unfiled";
   Email_Status_New      = "New";
   Email_Status_Read     = "Read";
@@ -17,6 +13,10 @@ class Email < ActiveRecord::Base
                             Email_Status_Read,
                             Email_Status_Ignored];
     
+  validates_presence_of :sender, :timestamp, :contents, :status; #:contactemail
+  validates_inclusion_of :status, :in => Email_Status_Group_All;
+  validates_format_of :sender, :with => Event::EmailRegex;
+
   def headerless_contents
     segments = contents.split(/\n[\r]*\n/);
     contents = segments[2, segments.size()].join("\n\n");

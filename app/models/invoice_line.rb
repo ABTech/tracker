124a1a83d8d484d01f15d1638f7eebf8177da3ea
Edward Garbade <tedgarb2@gmail.com>
2013-11-08 12:20:01 -0500

    reordering in invoice lines

--- app/models/invoice_line.rb
+++ app/models/invoice_line.rb
@@ -1,16 +1,16 @@
 class InvoiceLine < ActiveRecord::Base
   belongs_to :invoice
 
-  validates_presence_of :invoice_id, :price, :quantity, :category, :memo;
-  validates_inclusion_of :category, :in => Invoice_Categories;
-  validates_inclusion_of :category, :in => Invoice_Categories;
-
   Invoice_Categories = [  "Sound",
                           "Lighting",
                           "Reimbursement",
                           "Itemized",
                           "Labor",
                           "Event"]
+
+  validates_presence_of :invoice_id, :price, :quantity, :category, :memo;
+  validates_inclusion_of :category, :in => Invoice_Categories;
+  validates_inclusion_of :category, :in => Invoice_Categories;
 
   def total
     if (price and quantity)

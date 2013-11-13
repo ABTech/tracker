14356cf953e65a29ab729ead22f4abcafb38fee8
Edward <tedgarb2@gmail.com>
2013-11-12 21:17:40 -0500

    Rewrote all the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,43 +1,53 @@
 Abtt::Application.routes.draw do
-  resources :permissions
-  resources :roles
-  resources :locations
-  resources :comments
-  resources :members do
-    collection do
-      get 'edit_self'
-    end
-    member do
-      get 'settings'
-    end
-  end
   resources :accounts do
     collection do
-      get 'list'
+      get 'confirm_paid'
+      get 'events'
       get 'unpaid'
       get 'unpaid_print'
-      get 'events'
-    end
-  end
-
-  get 'calendar/generate.:format' => 'events#generate'
-  get 'calendar' => 'events#calendar'
-  get 'mobile' => 'events#mobile'
-  get 'iphone' => 'events#iphone'
-  get 'i' => 'events#iphone'
-  post 'invoice/email/:id' => 'invoice#email'
-
-  resources :organizations
-  resources :bugs
-  resources :tshirts
-  get 'invoice/list'
-  resources :invoice
-  resources :timecard_entries, except: :show
-  resources :timecards do
-    member do
       get 'view'
     end
   end
+
+  resources :attachments, only: [:destroy, :index]
+
+  resources :comments, only: [:create, :destroy]
+
+  resources :email, only: [:view] do
+    collection do
+      get 'file'
+      get 'list'
+      get 'mark_status'
+      get 'new_thread'
+      get 'pull_email'
+      get 'reply_to'
+      get 'send_email'
+      get 'unfile'
+    end
+  end
+
+  resources :email_forms, except: [:index] do
+    collection do
+      get 'list'
+    end
+  end
+
+  resources :equipment, only: [] do
+    collection do
+      get 'delgroup'
+      get 'delitem'
+      get 'editgroup'
+      get 'edititem'
+      get 'newgroup'
+      get 'newitem'
+      get 'savegroup'
+      get 'saveitem'
+      get 'tree'
+      get 'treesave'
+      get 'usage'
+    end
+  end
+
   resources :events do
     member do
       get 'delete_conf'
@@ -51,17 +61,69 @@ Abtt::Application.routes.draw do
     end
   end
 
-  resources :attachments, only: [:index, :destroy]
+  resources :heartbeat, only: [:index]
 
-  resources :version, only: [:show]
+  resources :invoice, only: [:create, :edit, :new] do
+    collection do
+      get 'email'
+      get 'email_confirm'
+      get 'list'
+      get 'prettyView'
+      get 'view'
+    end
+  end  
+
+  resources :invoice_items, except: [:show] do
+    collection do
+      get 'list'
+    end
+  end
+
+  resources :journal, except: [:show, :update] do
+    collection do
+      get 'list'
+    end
+  end
+
+  resources :locations
+
+  resources :member_filter, only: [:edit, :new, :save]
+
+  resources :members do
+    collection do
+      get 'edit_self'
+    end
+    member do
+      get 'settings'
+    end
+  end
+
+  resources :organizations
+
+  resources :permissions
+
+  resources :timecards do
+    member do
+      get 'view'
+    end
+  end
+
+  resources :timecard_entries, except: :show
+
+  resources :tshirts, only: :index
+
+  resources :version, only: :show
 
   post 'login' => 'useraccount#login'
   get 'logout' => 'useraccount#logout'
-
   get 'useraccount/access_denied'
-  root to: 'events#index'
 
-  get 'email/list'
-  get 'equipment/tree'
-  get 'invoice_items/index'
+  get 'calendar/generate.:format' => 'events#generate'
+  get 'calendar' => 'events#calendar'
+  get 'mobile' => 'events#mobile'
+  get 'iphone' => 'events#iphone'
+  get 'i' => 'events#iphone'
+  post 'invoice/email/:id' => 'invoice#email'
+
+  root to: 'events#index'
 end

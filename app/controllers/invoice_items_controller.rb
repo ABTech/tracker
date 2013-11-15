11f3d7819a15c35b742c7f781e00727f160df4c4
Edward <tedgarb2@gmail.com>
2013-11-15 01:17:22 -0500

    Invoice Items scare me

--- app/controllers/invoice_items_controller.rb
+++ app/controllers/invoice_items_controller.rb
@@ -1,4 +1,4 @@
-class InvoiceItemController < ApplicationController
+class InvoiceItemsController < ApplicationController
   def index
     list
   end
@@ -8,19 +8,19 @@ class InvoiceItemController < ApplicationController
   #       :redirect_to => { :action => :list }
 
   def list
-    @invoice_items = InvoiceItem.all
+    @invoice_items = InvoiceItems.all
     render :action=>'list', :layout=>"application2"
   end
 
   def new
-    @invoice_items = InvoiceItem.new
+    @invoice_items = InvoiceItems.new
     render :layout=>"application2"
   end
 
   def create
-    @invoice_items = InvoiceItem.new(params[:invoice_items])
+    @invoice_items = InvoiceItems.new(params[:invoice_items])
     if @invoice_items.save
-      flash[:notice] = 'InvoiceItem was successfully created.'
+      flash[:notice] = 'InvoiceItems was successfully created.'
       redirect_to :action => 'list'
     else
       render :action => 'new', :layout=>"application2"
@@ -28,14 +28,14 @@ class InvoiceItemController < ApplicationController
   end
 
   def edit
-    @invoice_items = InvoiceItem.find(params[:id])
+    @invoice_items = InvoiceItems.find(params[:id])
     render :layout=>"application2"
   end
 
   def update
-    @invoice_items = InvoiceItem.find(params[:id])
+    @invoice_items = InvoiceItems.find(params[:id])
     if @invoice_items.update_attributes(params[:invoice_items])
-      flash[:notice] = 'InvoiceItem was successfully updated.'
+      flash[:notice] = 'InvoiceItems was successfully updated.'
       redirect_to :action => 'show', :id => @invoice_items
     else
       render :action => 'edit', :layout=>"application2"
@@ -43,7 +43,7 @@ class InvoiceItemController < ApplicationController
   end
 
   def destroy
-    InvoiceItem.find(params[:id]).destroy
+    InvoiceItems.find(params[:id]).destroy
     redirect_to :action => 'list'
   end
 end

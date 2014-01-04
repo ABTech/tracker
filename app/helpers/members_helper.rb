f8684be9fc50cffc11d7f671d537c89b0db44d21
Edward <tedgarb2@gmail.com>
2014-01-03 19:22:12 -0500

    Fix headers on member table

--- app/helpers/members_helper.rb
+++ app/helpers/members_helper.rb
@@ -10,10 +10,10 @@ module MembersHelper
       else
         text += ' <img src="/images/sort_down.gif" class="order" alt="&darr;" />'
       end
-      return link_to(text, { :order => order_key, :desc => (order.include?("DESC") ? "0" : "1") })
+      return link_to(text.html_safe, { :order => order_key, :desc => (order.include?("DESC") ? "0" : "1") })
     else
       text += ' <img src="/images/sort_none.gif" class="order" alt="&harr;" />'
-      return link_to(text, { :order => order_key, :desc => (order.include?("DESC") ? "1" : "0") })
+      return link_to(text.html_safe, { :order => order_key, :desc => (order.include?("DESC") ? "1" : "0") })
     end
   end
 end

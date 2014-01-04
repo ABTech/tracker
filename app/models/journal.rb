56eafaefc150985eba4e2b3e9c26a8e4852e76a3
Edward <tedgarb2@gmail.com>
2014-01-04 18:51:21 -0500

    various journal fixes

--- app/models/journal.rb
+++ app/models/journal.rb
@@ -4,6 +4,8 @@ class Journal < ActiveRecord::Base
   belongs_to :event, :class_name => "Event", :foreign_key => "event_id";
   has_many :attachments
 
+  attr_accessible :date, :memo, :invoice_id, :amount, :date_paid, :notes, :account_id, :event_id, :paymeth_category
+
   validates_presence_of :date, :memo, :amount, :account;
   def date_s
     date.strftime('%B %d %Y')

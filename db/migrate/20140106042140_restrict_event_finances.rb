9a095da799392143facb879186b526dbc840017a
Kelly Rauchenberger <fefferburbia@gmail.com>
2014-01-06 00:33:46 -0500

    Fixed event finances restriction (so that it applies to Alumni too)

--- db/migrate/20140106042140_restrict_event_finances.rb
+++ db/migrate/20140106042140_restrict_event_finances.rb
@@ -1,13 +1,11 @@
 class RestrictEventFinances < ActiveRecord::Migration
   def up
+    p = Permission.new
+    p.pattern = "events/(show|show_email|new|create|edit|update|destroy|delete_conf|filtered_events|index|calendar_full|iphone|mobile|mobile_email|calendar|generate|lost)"
+    p.save!
+    
     Role.where(id: [2, 10]).each do |gen|
-      gen = Role.active
       gen.permissions.delete(Permission.where(pattern: "events/*").first)
-    
-      p = Permission.new
-      p.pattern = "events/(show|show_email|new|create|edit|update|destroy|delete_conf|filtered_events|index|calendar_full|iphone|mobile|mobile_email|calendar|generate|lost)"
-      p.save!
-    
       gen.permissions << p
       gen.save!
     end

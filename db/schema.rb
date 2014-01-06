022f7c8152b703a34448017b47a87879aeab64b8
Kelly Rauchenberger <fefferburbia@gmail.com>
2014-01-05 21:34:41 -0500

    Retroactively associated invoice journals with their events

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140106020832) do
+ActiveRecord::Schema.define(version: 20140106022838) do
 
   create_table "accounts", force: true do |t|
     t.string   "name",       null: false

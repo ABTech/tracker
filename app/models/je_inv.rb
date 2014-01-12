036bf47f6ef4ad91582b2738a08391784c275cdf
Kelly Rauchenberger <fefferburbia@gmail.com>
2014-01-12 02:53:58 -0500

    Removed Journal single table inheritance
    The journal table does not contain a type column and there is no difference between the different types of Journal anywhere.

--- app/models/je_inv.rb
+++
@@ -1,4 +0,0 @@
-class JeInv < Journal
-  belongs_to :invoice, :foreign_key => "link_id";
-  belongs_to :je_paid, :foreign_key => "link_paid_id";
-end
--- app/models/je_inv_paid.rb
+++
@@ -1,3 +0,0 @@
-class JeInvPaid < Journal
-  belongs_to :invoice, :foreign_key => "link_id";
-end

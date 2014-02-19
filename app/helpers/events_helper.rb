a735ac8a810f9e1765339343d29213c2e035b12e
Kelly Rauchenberger <fefferburbia@gmail.com>
2014-02-18 20:45:25 -0500

    Removed a few unnecessary helpers

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -32,25 +32,9 @@ module EventsHelper
     render :partial => "events/monthview", :locals => {monthdates: monthdates, show_arrows: show_arrows, selected: month, published: published}
   end
   
-  def my_comment?(member)   
-    member==current_member
-  end
-  
   def organizations_for_select
     Organization.active.order("name ASC").all.to_a.map do |org|
       [org.name, org.id]
-    end
-  end
-  
-  def statuses_for_select
-    Event::Event_Status_Group_All.map do |status|
-      [status, status]
-    end
-  end
-  
-  def roles_for_select
-    ([""] | EventRole::Roles_All).map do |role|
-      [role, role]
     end
   end
   

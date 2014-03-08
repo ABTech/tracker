948a1dc937b60d1068c4a53ef6e3e2f2367c006f
Edward <tedgarb2@gmail.com>
2014-03-08 16:19:12 -0500

    Show eventdate_roles for all eventdates

--- app/models/eventdate_role.rb
+++ app/models/eventdate_role.rb
@@ -1,4 +1,105 @@
 class EventdateRole < ActiveRecord::Base
   belongs_to :eventdate
   belongs_to :member
+
+  Role_HoT        = "HoT"
+  Role_TIC        = "TIC"
+  Role_aTIC       = "aTIC"
+  Role_FoH        = "FoH"
+  Role_aFoH       = "aFoH"
+  Role_Mon        = "Mon"
+  Role_aMon       = "aMon"
+  Role_LD         = "LD"
+  Role_aLD        = "aLD"
+  Role_Lprog      = "Lprog"
+  Role_ME         = "ME"
+  Role_aME        = "aME"
+  Role_MR         = "MR"
+  Role_aMR        = "aMR"
+  Role_SM         = "SM"
+  Role_aSM        = "aSM"
+  Role_bdSM       = "bdSM"
+  Role_SpotOp     = "SpotOp"
+  Role_Runner     = "Runner"
+  Role_Hole       = "Hole"
+  Role_aHole      = "aHole"
+  Role_exec       = "exec"
+  Role_car        = "car"
+  Role_trunk      = "truck"
+  Role_setup      = "setup"
+  Role_strike     = "strike"
+  Role_food       = "food"
+    
+  #Roles_all is also used for ordering roles (sorting)
+  Roles_All = [
+    Role_HoT    ,
+    Role_TIC    ,
+    Role_aTIC   ,
+    Role_FoH    ,
+    Role_aFoH   ,
+    Role_Mon    ,
+    Role_aMon   ,
+    Role_LD     ,
+    Role_aLD    ,
+    Role_Lprog  ,
+    Role_ME     ,
+    Role_aME    ,            
+    Role_MR     ,            
+    Role_aMR    ,            
+    Role_SM     ,            
+    Role_aSM    ,            
+    Role_bdSM   ,            
+    Role_SpotOp ,            
+    Role_Runner ,            
+    Role_Hole   ,            
+    Role_aHole  ,            
+    Role_exec   ,
+    Role_car    ,     
+    Role_trunk  ,          
+    Role_setup  ,
+    Role_strike ,
+    Role_food   ]
+
+  validates_presence_of :role
+  validates_inclusion_of :role, :in => Roles_All
+
+  def assigned?
+    return (member != nil);
+  end
+
+  def to_s
+    role + ": " + assigned_to
+  end
+  
+  def assigned_to
+    if assigned?
+      member.fullname
+    else
+      "(unassigned)"
+    end
+  end
+
+  def sort_index 
+    Roles_All.each_index { |role_index| return role_index if Roles_All[role_index] == role }
+    return -1 
+  end
+
+  # define the natural sorting order
+  def <=> (role)
+    return 1 if sort_index < 0
+    return -1 if role.sort_index < 0
+    return sort_index <=> role.sort_index   
+  end
+  
+  def assistants
+    return [Role_aTIC] if self.role == Role_TIC
+    return [Role_aFoH] if self.role == Role_FoH
+    return [Role_aMon] if self.role == Role_Mon
+    return [Role_aLD, Role_Lprog] if self.role == Role_LD
+    return [Role_aME] if self.role == Role_ME
+    return [Role_aMR] if self.role == Role_MR
+    return [Role_aSM, Role_bdSM] if self.role == Role_SM
+    return [Role_aHole] if self.role == Role_Hole
+    return []
+  end
 end

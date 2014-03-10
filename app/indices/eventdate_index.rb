f1f5d4e2989c4ef8e46a65c0b109fc789b4ecae4
Edward <tedgarb2@gmail.com>
2014-03-09 22:07:24 -0400

    Search by venue

--- app/indices/eventdate_index.rb
+++ app/indices/eventdate_index.rb
@@ -3,6 +3,9 @@ ThinkingSphinx::Index.define :eventdate, :with => :active_record do
   indexes description, :sortable => true
   indexes event.title, :as => :event
   indexes event.organization.name, :as => :organization
+  indexes locations.building, :as => :location_building
+  indexes locations.floor, :as => :location_floor
+  indexes locations.room, :as => :location_room
 
   has startdate
 end

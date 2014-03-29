e92966af35f9eb354445f8d37e2a5769c64d785f
Kelly Rauchenberger <fefferburbia@gmail.com>
2014-03-29 00:16:41 -0400

    Revert "Convert equipment_eventdates to equipment_events"
    This reverts commit 42e72989fbca244a5063b5988cc26af9b33e46fc.
    
    Conflicts:
    	db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140309232438) do
+ActiveRecord::Schema.define(version: 20140309175347) do
 
   create_table "accounts", force: true do |t|
     t.string   "name",       null: false

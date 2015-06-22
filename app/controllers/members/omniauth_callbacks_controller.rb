21bad7df2be800042b76b63a25573b7fba9b331d
Kelly Rauchenberger <fefferburbia@gmail.com>
2015-06-22 17:19:44 -0400

    Fixed issue with omniauth/shib interaction

--- app/controllers/members/omniauth_callbacks_controller.rb
+++ app/controllers/members/omniauth_callbacks_controller.rb
@@ -1,7 +1,7 @@
 class Members::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def shibboleth
-    @member = Member.find_by_email(request["omniauth.auth"].uid)
+    @member = Member.find_by_email(request.env["omniauth.auth"].uid)
     if @member
       sign_in_and_redirect @member, :event => :authentication
       set_flash_message(:notice, :success) if is_navigational_format?

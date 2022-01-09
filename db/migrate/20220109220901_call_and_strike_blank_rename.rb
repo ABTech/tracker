32514fb48ce4858049ed9c3391361dcb64326a77
Perry Naseck <git@perrynaseck.com>
2022-01-09 18:44:34 -0500

    confirmed migration works, remove warning

--- db/migrate/20220109220901_call_and_strike_blank_rename.rb
+++ db/migrate/20220109220901_call_and_strike_blank_rename.rb
@@ -3,17 +3,11 @@ class CallAndStrikeBlankRename < ActiveRecord::Migration[6.0]
     execute "UPDATE eventdates SET striketype = 'blank_strike' WHERE striketype = 'blank'"
     execute "UPDATE eventdates SET calltype = 'blank_call' WHERE calltype = 'blank';"
     change_column :eventdates, :calltype, :string, default: "blank_call"
-    puts "Trust me, it didn't run this migration correctly. Go to the SQL console and run:"
-    puts "UPDATE eventdates SET striketype = 'blank_strike' WHERE striketype = 'blank';"
-    puts "UPDATE eventdates SET calltype = 'blank_call' WHERE calltype = 'blank';"
   end
 
   def down
     execute "UPDATE eventdates SET striketype = 'blank' WHERE striketype = 'blank_strike';"
     execute "UPDATE eventdates SET calltype = 'blank' WHERE calltype = 'blank_call';"
     change_column :eventdates, :calltype, :string, default: "blank"
-    puts "Trust me, it didn't run this migration correctly. Go to the SQL console and run:"
-    puts "UPDATE eventdates SET striketype = 'blank' WHERE striketype = 'blank_strike';"
-    puts "UPDATE eventdates SET calltype = 'blank' WHERE calltype = 'blank_call';"
   end
 end

213f7522c8c561a460ba0968ae5583697032b827
Perry Naseck <git@perrynaseck.com>
2022-01-09 22:51:12 -0500

    update to Rails 6.1 and run app:update

--- config/application.rb
+++ config/application.rb
@@ -11,18 +11,18 @@ module Abtt
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
-    config.time_zone = 'America/New_York'
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
+    config.time_zone = 'Eastern Time (US & Canada)'
     
     config.to_prepare do
       Devise::SessionsController.layout "application"
       Devise::PasswordsController.layout "application"
     end
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
   end
 end

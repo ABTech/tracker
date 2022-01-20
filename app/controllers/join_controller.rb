e3602cd42d08327c4276ff93ffb0a365f4a34f05
stella5423890 <maristella1008@gmail.com>
2022-01-20 18:54:23 -0500

    Add join form

---
+++ app/controllers/join_controller.rb
@@ -0,0 +1,37 @@
+class JoinController < ApplicationController
+  def join
+    input = params.require([:andrew_id, :preferred_name, :last_name])
+
+    special = "?<>',?[]}{=-)(*&^%$#`~{}"
+    regex = /[#{special.gsub(/./){|char| "\\#{char}"}}]/
+    form_error = [] 
+
+    if params['andrew_id'] =~ regex || params['andrew_id'].length >10 || params['andrew_id'].length < 2
+      form_error<<"Invalid Andrew ID!"
+      
+    end
+
+
+    if params['preferred_name'].blank? || params['last_name'].blank?
+      form_error<<"Please enter your name."
+    
+    end 
+      
+    if params['andrew_id'].blank?
+      form_error<<"Please enter your Andrew ID."
+    end
+    
+    if form_error.length > 0
+      render json: form_error, status: 400
+    else
+      begin
+         JoinRequestMailer.join_request(params['andrew_id'], params['preferred_name'], params['last_name']).deliver_now
+      rescue
+         head 500
+      else
+         head 200
+      end
+    end  
+
+  end
+end

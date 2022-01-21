b747593cfd3083204ede4aafe552fa66c0147e1d
Perry Naseck <git@perrynaseck.com>
2022-01-20 21:47:59 -0500

    fix join form indent, no token or auth on join form

--- app/controllers/join_controller.rb
+++ app/controllers/join_controller.rb
@@ -1,24 +1,27 @@
 class JoinController < ApplicationController
-  def join
+  skip_before_action :authenticate_member!, :only => [:joinrequest]
+  skip_before_action :verify_authenticity_token, :only => [:joinrequest]
+
+  def joinrequest
     input = params.require([:andrew_id, :preferred_name, :last_name])
 
     special = "?<>',?[]}{=-)(*&^%$#`~{}"
     regex = /[#{special.gsub(/./){|char| "\\#{char}"}}]/
     form_error = [] 
 
-    if params['andrew_id'] =~ regex || params['andrew_id'].length >10 || params['andrew_id'].length < 2
-      form_error<<"Invalid Andrew ID!"
+    if params['andrew_id'] =~ regex || params['andrew_id'].length > 10 || params['andrew_id'].length < 2
+      form_error << "Invalid Andrew ID!"
       
     end
 
 
     if params['preferred_name'].blank? || params['last_name'].blank?
-      form_error<<"Please enter your name."
+      form_error << "Please enter your name."
     
     end 
       
     if params['andrew_id'].blank?
-      form_error<<"Please enter your Andrew ID."
+      form_error << "Please enter your Andrew ID."
     end
     
     if form_error.length > 0

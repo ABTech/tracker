4886095d0eeff21d5b5f4dfff8a7147922c64b75
Perry Naseck <git@perrynaseck.com>
2022-10-01 15:01:35 -0400

    make invoice contact email immutable (see #487)

--- app/models/invoice_contact.rb
+++ app/models/invoice_contact.rb
@@ -1,6 +1,7 @@
 class InvoiceContact < ApplicationRecord
   validates_format_of :email, with: Event::EmailRegex, multiline: true
   validates :email, uniqueness: true
+  attr_readonly :email
 
   PERMANENT_INVOICE_CONTACTS = [
     'SLICEfinance@andrew.cmu.edu',

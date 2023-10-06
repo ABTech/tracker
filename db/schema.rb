a01fc5b51099a8161f4fe97a1d8066b926f9799b
wjiang42 <wellington.jiang@indiansprings.org>
2023-10-06 16:43:12 -0400

    Update .gitignore and schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -30,18 +30,6 @@ ActiveRecord::Schema.define(version: 2023_08_01_123836) do
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
-  create_table "active_storage_blobs", charset: "utf8mb4", collation: "utf8mb4_bin", force: :cascade do |t|
-    t.string "key", null: false
-    t.string "filename", null: false
-    t.string "content_type"
-    t.text "metadata"
-    t.string "service_name", null: false
-    t.bigint "byte_size", null: false
-    t.string "checksum", null: false
-    t.datetime "created_at", null: false
-    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
-  end
-
   create_table "active_storage_variant_records", charset: "utf8mb4", collation: "utf8mb4_bin", force: :cascade do |t|
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false

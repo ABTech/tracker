511b708d80e9daa670f0a87f435aad87504e5417
Tucker Shea <tucker@tuckershea.com>
2025-03-22 18:51:24 -0400

    Back out "Make dev use sqlite. BREAKS SEARCH"
    This backs out commit b4c632f2a704d837bce8a4c785fab87f7e2a0cff.

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'grover'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(:default, Rails.env)
+Bundler.require(*Rails.groups)
 
 module Abtt
   class Application < Rails::Application
